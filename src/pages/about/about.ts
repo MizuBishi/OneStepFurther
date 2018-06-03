1c4e583f3169910393e8093a36c6effd41945966
Mizu <>
2018-06-03 20:06:25 +0200

    Copy Charts from the other old project.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Chart } from "chart.js";
 
 @Component({
   selector: 'page-about',
@@ -9,6 +10,50 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  @ViewChild("doughnutCanvas") doughnutCanvas;
+  doughnutChart: any;
+  actualDate: any;
+
+
+  ionViewDidLoad() {
+    this.actualDate = new Date().toISOString().slice(0, 10);
+    
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            data: [12, 19],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+            ],
+            hoverBackgroundColor: ["#FF6384", "#36A2EB",],
+            borderWidth: [0,0],
+          }
+        ],
+        labels: ["Michelle", "Ronia"]
+      },
+      options: {
+        legend: {
+            display: true,
+            position: 'bottom',
+            fullWidth: true,
+            labels: {
+              fontSize: 20,
+            }
+        },
+        title: {
+          display: true,
+          fontSize: 25,
+          position: 'top',
+          fontFamily: 'Helvetica Neue',
+          padding: 15,
+        }
+      }
+    });
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 import { NavController } from 'ionic-angular';
+import Chart from 'chart.js';
+
 
 @Component({
   selector: 'page-contact',
@@ -10,5 +12,34 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
 
   }
+@ViewChild('barCanvas') barCanvas;
 
+  barChart: any;
+  ionViewDidLoad() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+			type: 'horizontalBar',
+			data: {
+				datasets: [{
+					data: [12, 19, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+					],
+					borderWidth: 1
+				}],
+				labels: ["Michelle", "Ronia", "Tim"],
+			},
+			options: {
+				legend: {
+					display: false
+				},
+			}
+    });
+  }
 }
